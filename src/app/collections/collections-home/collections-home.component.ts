146e964b3a72b0029b01aeace27a4b1b5a1ea916
Ashbeel Ghouri <ash.ghouri@techtronix.biz>
2020-12-15 11:19:16 +0500

    added the tables shared widget

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data = [
+    { name: "james", age: 25, job: "designer", employed: true },
+    { name: "jill", age: 19, job: "engineer", employed: false },
+    { name: "elyse", age: 32, job: "engineer", employed: true }
+  ];
+
+  headers = [
+    {key: "employed", label: "Has a Job?"},
+    { key: "name", label: "Name" },
+    { key: 'age', label: "Age" },
+    { key: "job", label: "Job" }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
